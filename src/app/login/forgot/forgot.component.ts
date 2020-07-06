4e17220dbbe4b488acc1a30e91e62d9a35455b52
Steven Glasford <stevenglasford@gmail.com>
2020-07-06 17:22:41 -0500

    Add Styling

--- src/app/login/forgot/forgot.component.ts
+++ src/app/login/forgot/forgot.component.ts
@@ -12,6 +12,14 @@ export class ForgotComponent implements OnInit {
   forgotForm: FormGroup;
   ServerMessage: string = '';
 
+  error_messages = {
+    'email':[
+      { type: 'required', message: 'Email is Required.'},
+      { type: 'valid', message: 'A valid email is required.'},
+      { type: 'minlength', message: 'Email length.' },
+      { type: 'required', message: 'please enter a valid email address.' }
+    ]
+  }
 
   constructor(
     private fb: FormBuilder,
@@ -24,6 +32,8 @@ export class ForgotComponent implements OnInit {
     this.forgotForm = this.fb.group({
       email: new FormControl('',Validators.compose([
         Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
       ]))
     })
   }
