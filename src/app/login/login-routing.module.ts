dfef31cace160d6116f4c613ca3f829b1c906758
stevenglasford <stevenglasford@gmail.com>
2020-07-06 12:10:54 -0500

    Begin work on adding firebase authentication

---
+++ src/app/login/login-routing.module.ts
@@ -0,0 +1,48 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class LoginRoutingModule { }
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { HomeComponent } from './home/home.component'
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: MainComponent,
+    children: [
+      {
+        path: 'forgot',
+        component: ForgotComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'home',
+        component:HomeComponent
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class LoginRoutingModule { }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+
+
 import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotComponent } from './forgot/forgot.component';
-
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [MainComponent, RegisterComponent, ForgotComponent],
+  declarations: [
+    MainComponent, 
+    RegisterComponent, 
+    ForgotComponent, 
+    HomeComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
